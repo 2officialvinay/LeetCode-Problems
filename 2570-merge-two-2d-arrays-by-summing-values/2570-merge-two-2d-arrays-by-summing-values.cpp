baf69c28b8d121c2c183bcf762af9327bf214cba
Vinay Saini <2officialvinay@gmail.com>
2026-08-02 18:12:09 +0530

    Time: 0 ms (100%), Space: 14.7 MB (60.83%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,21 +2,51 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& num1, vector<vector<int>>& num2) {
         vector<vector<int>> ans;
-        unordered_map<int,int> mp;
 
-        for(int i = 0; i < num1.size(); i++){
-            mp[num1[i][0]] += num1[i][1];
+        int n1 = num1.size();
+        int n2 = num2.size();
+
+        int i = 0, j = 0;
+        int id1, id2;
+
+        while(i < n1 && j < n2){
+            id1 = num1[i][0];
+            id2 = num2[j][0];
+
+            if(id1 == id2){
+                int val = num1[i][1] + num2[j][1];
+                ans.push_back({id1, val});
+                i++;
+                j++;
+            }
+
+            else if(id1 < id2){
+                int val = num1[i][1];
+                ans.push_back({id1, val});
+                i++;
+            }
+
+            else{
+                int val = num2[j][1];
+                ans.push_back({id2, val});
+                j++;
+            }
         }
 
-        for(int i = 0; i < num2.size(); i++){
-            mp[num2[i][0]] += num2[i][1];
+        while(i < n1){
+            id1 = num1[i][0];
+            int val = num1[i][1];
+            ans.push_back({id1, val});
+            i++;
         }
 
-        for(auto it:mp){
-            ans.push_back({it.first, it.second});
+        while(j < n2){
+            id2 = num2[j][0];
+            int val = num2[j][1];
+            ans.push_back({id2, val});
+            j++;
         }
 
-        sort(ans.begin(), ans.end());
         return ans;
     }
 };
